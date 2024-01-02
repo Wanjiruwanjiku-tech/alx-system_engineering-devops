0af896118a0c80339e61780fd7a7342c84344223
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2024-01-02 18:58:01 +0300

    CSV format

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,12 +8,10 @@ import csv
 import requests
 import sys
 
+
 if __name__ == "__main__":
     # Define the base url for the Api
     url = "https://jsonplaceholder.typicode.com/"
-    
-
-
 
     # Check if the correct number of args is provided
     if len(sys.argv) != 2:
@@ -26,47 +24,15 @@ if __name__ == "__main__":
     # Fetch user data from the API
     user_response = requests.get(url + "users/{}".format(employee_id))
     user = user_response.json()
+    username = user.get("username")
 
     # Fetch todos for the employee
     params = {"userId": employee_id}
     todos_response = requests.get(url + "todos?userId={}".format(employee_id))
     todos = todos_response.json()
 
-    # Extract the completed tasks
-    completed_tasks =[]
+    with open("{}.csv".format(employee_id), "w", newline="") as csv_file:
+        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
 
-    for todo in todos:
-        if todo.get("completed") is True:
-            completed_tasks.append(todo.get("title"))
-    
-    # Dispay the information
-    print("Employee {} is done with tasks({}/{}):".format(user.get("name"), len(completed_tasks), len(todos)))
-
-    for completed_task in completed_tasks:
-        print("\t {}".format(completed_task))
-
-
-    # CSV Format
-    def export_to_csv(user_id, user_name, tasks):
-        """Export completed tasks to a CSV File.
-        
-        Keyword arguments:
-        user_id -- the id of the employee
-        user_name -- the user's name
-        tasks -- other tasks (total)
-        """
-        
-        csv_filename = "{}.csv".format(user_id)
-        with open(csv_filename, mode='w', newline='') as csv_file:
-            fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
-            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-            writer.writeheader()
-            
-            for task in tasks:
-                writer.writerow({
-                    'USER_ID': user_id,
-                    'USERNAME': user_name,
-                    'TASK_COMPLETED_STATUS': task.get('completed'),
-                    'TASK_TITLE': task.get('title')
-                })
-        print("Tasks exported to {}".format(csv_filename))
+        for todo in todos:
+            writer.writerow([employee_id, username, todo.get("completed"), todo.get("title")])
