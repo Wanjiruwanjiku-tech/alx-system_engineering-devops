deeab01562876eb99a078b267ca841a30544ea45
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2024-01-16 20:44:55 +0300

    Task 0
    Query the reddit API to get the number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+The Module querries the reddit api and returns the number of subscribers
+"""
+# Import required modules
+import requests
+
+def number_of_subscribers(subreddit):
+    """ The function uses requests to gather info
+    for a given subreddit
+    """
+    # Define a custom User-Agent in the headers
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+
+    # Make a GET request to reddit using the specified subreddit
+    response = requests.get('https://www.reddit.com/r/{:}/about.json'.format(
+        subreddit), headers=headers, allow_redirects=False)
+    
+    # Check the response status
+    if response >= 300:
+        return 0
+    
+    # Parse the Jason Response
+    json_body = response.json()
+
+    # Extarct the data from json_body
+    data = json_body.get('data')
+    return(data.get('subscribers'))
